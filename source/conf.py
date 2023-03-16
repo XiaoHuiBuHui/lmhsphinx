be5e2c71a85d88e26f1604511e88081e835a3fb2
minghui <1229660539@qq.com>
2023-03-16 16:50:12 +0800

    init

--- source/conf.py
+++ source/conf.py
@@ -6,23 +6,47 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+# project = 'lmhsphinx'
+# copyright = '2023, lmh'
+# author = 'lmh'
+# release = 'v2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# extensions = []
+
+# templates_path = ['_templates']
+# exclude_patterns = []
+
+# language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
+
+# ---------------------------自定义------------------------
 project = 'lmhsphinx'
 copyright = '2023, lmh'
 author = 'lmh'
 release = 'v2'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# ---------支持md---------https://www.osgeo.cn/sphinx/usage/markdown.html#id7
+extensions = ['myst_parser']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
-extensions = []
+
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'zh_CN'
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
